b305f3ec08d07c8bf04e5d0060b36facb1c52953
forrestz17 <1607808325@qq.com>
2021-07-27 14:09:20 +0800

    version1

--- Character.py
+++ Character.py
@@ -1,28 +1,54 @@
 import pygame
-
+import random
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, cha_surf):
+        pygame.sprite.Sprite.__init__(self)
+        self.move_left = False
+        self.move_right = False
+        self.move_up = False
+        self.move_down = False
+        self.loc_x = 20
+        self.loc_y = 30
         self.hv = 5
         self.vv = 5
-        self.size_x = 20
-        self.size_y = 30
-        self.gadget = None
-
-    def update(self):
-        pass
 
 
-class PlayerOne(Player):
+        self.surf = pygame.Surface((10,15))
+        self.surf.blit(cha_surf, (0,0), (32, 32, 10, 15))
+        self.rect = self.surf.get_rect()
+
+        '''self.size_x = 30
+        self.size_y = 40
+        self.loc_x = pos_x
+        self.loc_y = pos_y
+        self.image = pygame.Surface((self.loc_x,self.loc_y))
+        self.image.blit(image_surf, (0, 0), (4, 13, 29, 27))'''
+
+
+
+    def update(self, size_x, size_y ):
+        if self.move_left :
+            self.rect.move_ip(-1,0)
+        if self.move_right :
+            self.rect.move_ip(1,0)
+        if self.move_up:
+            self.rect.move_ip(0,-1)
+        if self.move_down:
+            self.rect.move_ip(0,1)
+
+        if self.rect.top>size_y or self.rect.bottom<0 or self.rect.left>size_x or self.rect.right<0:
+            self.kill()
+
+
+
+
+
+
+
+
+class Gadgets(pygame.sprite.Sprite):
     def __init__(self):
-        super().__init__()
-
-class PlayerTwo(Player):
-    def __init__(self):
-        super().__init__()
-
-
-class Gadgets:
-    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         self.appear = False
         self.size_x = 30
         self.size_y = 30
--- Controller.py
+++ Controller.py
@@ -1,6 +1,7 @@
 import pygame.gfxdraw
 import Maze
 import random
+import Character
 
 def main():
     pygame.init()
@@ -8,4 +9,95 @@ def main():
     WindowSize_y = 650
     surface = pygame.display.set_mode([WindowSize_x,WindowSize_y])
     pygame.display.set_caption('Catch FLAG')
-    background = pygame.image.load(r'').convert()
+    background = pygame.image.load(r'F:\cis\3\mario_background.png').convert()
+    image_surf = pygame.image.load(r'F:\cis\4\code\mario_sprites.png').convert()
+    surface.blit(background,(0,0))
+
+
+    player_group = pygame.sprite.Group
+    player_zero = Character.Player(image_surf)
+    player_group.add(player_zero)
+
+
+    clock= pygame.time.Clock()
+    running = True
+    markup = None
+    g = Maze.Grid(24, 32)
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                elif event.key == K_q:  # Quit
+                    running = False
+                elif event.key == K_n:  # New
+                    g = mazes.Grid(24, 32)
+                    markup = None
+                elif event.key == K_a:  # Aldous-Broder random walk
+                    mazes.aldous_broder(g)
+                    markup = None
+                elif event.key == pygame.K_LEFT:
+                    player1.move_left = True
+                elif event.key == pygame.K_RIGHT:
+                    player1.move_right = True
+                elif event.key == pygame.K_UP:
+                    player1.move_uo = True
+                elif event.key == pygame.K_DOWN:
+                    player1.move_down = True
+        surface.blit(background, (0, 0))
+        display_grid(g, markup, surface)
+
+        player_group.draw(surface)
+        player_group.update(WindowSize_x, WindowSize_y)
+        pygame.display.flip()
+
+
+def display_grid(g, markup, screen):
+    screen.fill((0, 0, 0))
+    for row in range(g.num_rows):
+        for col in range(g.num_columns):
+            c = g.cell_at(row, col)
+            cell_x = col * 32 + 5
+            cell_y = row * 32 + 5
+            # Draw top row
+            if markup:
+                value = markup.get_item_at(row, col)
+                if not value:
+                    continue
+                if value == '*':  # Path marker
+                    pygame.draw.circle(screen,
+                                       (150, 80, 50),
+                                       (cell_x + 15, cell_y + 15),
+                                       7,  # radius
+                                       0)  # filled
+                if isinstance(value, list) and len(value) == 3:
+                    pygame.draw.rect(screen,
+                                     value,  # color
+                                     (cell_x, cell_y, 32, 32))
+
+            if not c.north or not c.is_linked(c.north):
+                pygame.gfxdraw.hline(screen,
+                                     cell_x, cell_x + 31, cell_y,
+                                     (100, 100, 100))
+            if not c.south or not c.is_linked(c.south):
+                pygame.gfxdraw.hline(screen,
+                                     cell_x, cell_x + 31, cell_y + 31,
+                                     (100, 100, 100))
+            if not c.east or not c.is_linked(c.east):
+                pygame.gfxdraw.vline(screen,
+                                     cell_x + 31, cell_y, cell_y + 31,
+                                     (100, 100, 100))
+            if not c.west or not c.is_linked(c.west):
+                pygame.gfxdraw.vline(screen,
+                                     cell_x, cell_y, cell_y + 31,
+                                     (100, 100, 100))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
